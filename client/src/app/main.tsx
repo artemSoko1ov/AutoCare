ec212e3a045e6d050eee8aec7fbf672ac5f2f2f9
Artem Sokolov <the.jelll.2006@gmail.com>
2026-05-29 13:14:48 +0500

    feat(client): add basic Redux

--- client/src/app/main.tsx
+++ client/src/app/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/app/App";
 import "@/app/styles";
+import { StoreProvider } from "@app/providers/store/StoreProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <StoreProvider>
+      <App />
+    </StoreProvider>
   </StrictMode>,
 );
