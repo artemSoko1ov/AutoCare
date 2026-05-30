1b4548637a0d1562da3898b3ee5bab746d9f8be6
Artem Sokolov <the.jelll.2006@gmail.com>
2026-05-30 12:37:18 +0500

    feat(shared): add zod contracts and field username for model User

---
+++ shared/contracts/auth/login.contract.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+export const LoginBodySchema = z.object({
+  email: z.email('Некорректный email'),
+  password: z
+    .string()
+    .min(1, 'Введите пароль'),
+});
+
+export const LoginResponseSchema = z.object({
+  accessToken: z.string(),
+  user: z.object({
+    id: z.string(),
+    email: z.email(),
+    username: z.string(),
+  }),
+});
+
+export type LoginBody = z.infer<typeof LoginBodySchema>;
+export type LoginResponse = z.infer<typeof LoginResponseSchema>;
---
+++ shared/contracts/auth/register.contract.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+export const RegisterBodySchema = z.object({
+  email: z.email('Некорректный email'),
+  username: z
+    .string()
+    .min(3, 'Имя должно быть минимум 3 символа')
+    .max(20, 'Имя должно быть максимум 20 символов'),
+  password: z
+    .string()
+    .min(6, 'Пароль должен быть минимум 6 символов')
+    .max(64, 'Пароль должен быть максимум 64 символа'),
+});
+
+export const RegisterResponseSchema = z.object({
+  id: z.string(),
+  email: z.email(),
+  username: z.string(),
+});
+
+export type RegisterBody = z.infer<typeof RegisterBodySchema>;
+export type RegisterResponse = z.infer<typeof RegisterResponseSchema>;
