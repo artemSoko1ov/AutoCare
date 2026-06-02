445b621a91ade5b17d3b21fe063e9e8e5902ec91
Artem Sokolov <the.jelll.2006@gmail.com>
2026-06-02 14:05:04 +0500

    feat(server): add method register in auth service

--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -1,7 +1,59 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../../prisma/prisma.service';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from '@prisma/prisma.service';
+import { RegisterBody } from '@shared/contracts/auth';
+import * as bcrypt from 'bcryptjs';
+import { TokensService } from '../tokens/tokens.service';
+import { TokensDto } from '@shared/contracts/auth/tokens.contract';
+import { UserDto } from '@shared/contracts/auth/user.contract';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly tokenService: TokensService,
+  ) {}
+
+  async register({
+    email,
+    username,
+    password,
+  }: RegisterBody): Promise<TokensDto & UserDto> {
+    const candidate = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (candidate) {
+      throw new ConflictException(
+        `Пользователь с почтой ${email} уже существует`,
+      );
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        username,
+        password: hashPassword,
+      },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        createdAt: true,
+      },
+    });
+
+    const userWithDate = {
+      ...user,
+      createdAt: user.createdAt.toISOString(),
+    };
+
+    const tokens = this.tokenService.generateTokens(userWithDate);
+    await this.tokenService.saveToken(userWithDate.id, tokens.refreshToken);
+
+    return {
+      ...tokens,
+      ...userWithDate,
+    };
+  }
 }
