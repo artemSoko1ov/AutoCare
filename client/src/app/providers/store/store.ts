374da5e6d0743f01451dbb96a663cac5f3edde6d
Artem Sokolov <the.jelll.2006@gmail.com>
2026-06-03 08:06:30 +0500

    chore(client): add store configuration and typed hooks

--- client/src/app/providers/store/store.ts
+++ client/src/app/providers/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import sessionReducer from "@/entities/session/model";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    session: sessionReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
