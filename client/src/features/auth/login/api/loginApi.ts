a685067b15e8274310f1fe2f1c5c30b630628f10
Artem Sokolov <the.jelll.2006@gmail.com>
2026-06-03 09:06:56 +0500

    feat(client): add method loginApi

---
+++ client/src/features/auth/login/api/loginApi.ts
@@ -0,0 +1,7 @@
+import axiosInstance from "@/shared/api/axiosInstance.ts";
+import type { LoginBody, LoginResponse } from "@shared/contracts/auth";
+
+export const loginApi = async (data: LoginBody): Promise<LoginResponse> => {
+  const response = await axiosInstance.post<LoginResponse>("/auth/login", data);
+  return response.data;
+};
