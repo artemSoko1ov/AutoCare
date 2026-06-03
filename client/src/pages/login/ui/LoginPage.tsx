e25d34fc97d4cc94628a17c9c4b23eecdc82cfaf
Artem Sokolov <the.jelll.2006@gmail.com>
2026-06-03 12:48:32 +0500

    feat(client): connect login form to login page

--- client/src/pages/login/ui/LoginPage.tsx
+++ client/src/pages/login/ui/LoginPage.tsx
@@ -1,5 +1,12 @@
+import LoginForm from "@/features/auth/login";
+
 const LoginPage = () => {
-  return <h1>Вход</h1>;
+  return (
+    <>
+      <h1>Вход</h1>
+      <LoginForm />
+    </>
+  );
 };
 
 export default LoginPage;
